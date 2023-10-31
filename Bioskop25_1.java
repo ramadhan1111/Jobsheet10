a0ad10ff91ab286b626784aac8970ba12458a1b0
ramadhan1111 <144088505+ramadhan1111@users.noreply.github.com>
2023-10-31 11:45:24 +0700

    Add files via upload
    Modif soal 1 ke 3

--- Bioskop25_1.java
+++ Bioskop25_1.java
@@ -14,10 +14,10 @@ public class Bioskop25_1 {
         penonton[3][0]="Gisel";
         penonton[3][1]="Hana";
 
-        System.out.println(penonton.length);
+        System.out.println("Penonton pada baris ke-3");
        
-        for (int i = 0; i < penonton.length; i++){
-            System.out.println("Panjang baris ke-" + (i+1) + ":" + penonton[i].length);
+        for (int i = 0; i < penonton[2].length; i++){
+            System.out.println(penonton[2][i]);
         }
     }
 }
