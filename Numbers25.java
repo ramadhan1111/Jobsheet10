76eaec2603e4ac2feb898acbf539fdff9f048af8
ramadhan1111 <144088505+ramadhan1111@users.noreply.github.com>
2023-10-31 13:48:08 +0700

    Add files via upload

---
+++ Numbers25.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Numbers25 {
+    public static void main (String[] args){
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int [5];
+        myNumbers[1] = new int [3];
+        myNumbers[2] = new int [1];
+
+        System.out.println(myNumbers.length);
+        System.out.println(myNumbers[0].length);
+        System.out.println(myNumbers[1].length);
+        System.out.println(myNumbers[2].length);
+
+    } 
+}
